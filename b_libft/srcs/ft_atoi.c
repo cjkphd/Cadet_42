b7dda9a3c40aaadf6721be7977242e35fedd02d2
Matthew Mateo <mmatteo36@gmail.com>
2018-10-25 12:05:23 -0700

    modified atoi

--- b_libft/srcs/ft_atoi.c
+++ b_libft/srcs/ft_atoi.c
@@ -1,28 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "libft.h"
 
 int		ft_atoi(const char *str)
 {
 	int i;
-	int sym;
+	int sign;
 	int result;
 
 	i = 0;
-	sym = 1;
+	sign = 1;
 	result = 0;
-	//while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n') || (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
 	while ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			sym = -1;
+			sign = -1;
 		i++;
 	}
 	while ((str[i] >= '0') && (str[i] <= '9'))
-{
+	{
 		result = result * 10 + str[i] - '0';
 		i++;
 	}
-	return (result * sym);
+	return (result * sign);
 }
+
+/*
+**Optional Conditions for Discarding Whitespaces: 
+**while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
+**|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
+ */
