64795ec9e5fcc4dfe49d22e89a2e5b3339ae4633
Matthew Mateo <mmatteo36@gmail.com>
2018-10-30 09:30:02 -0700

    new file ftmemcpy

---
+++ b_libft/srcs/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/29 18:26:30 by mamateo           #+#    #+#             */
+/*   Updated: 2018/10/29 18:26:31 by mamateo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
+	char *here;
+	const char *from;
+
+	here = dst;
+	from = src;
+	i = 0;
+	while (i < n)
+	{
+		here[i] = from[i];
+		i++;
+	}
+	return (dst);
+}
