c9902934738940bf015eaed994bced13561089db
Matthew Mateo <mmatteo36@gmail.com>
2018-11-23 17:04:59 -0800

    modifications

--- b_libft/srcs/ft_memcmp.c
+++ b_libft/srcs/ft_memcmp.c
@@ -6,22 +6,30 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 14:36:05 by mamateo           #+#    #+#             */
-/*   Updated: 2018/11/02 14:42:43 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/11/23 17:01:54 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+int				ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
-	while (i < n)
+	while (n-- > 0)
 	{
-		if (((char *)s1)[i] != ((char *)s2)[i])
-			return (((char *)s1)[i] - ((char *)s2)[i]);
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
 	return (0);
 }
+
+/*
+** const unsigned char *str1 = s1;
+** const unsigned char *str2 = s2;
+** while (count-- > 0)
+** if (*s1++ != *s2++)
+** if s1[-1] is less than s2[-1] if true -1 otherwise 1
+*/
