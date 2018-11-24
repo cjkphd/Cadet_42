60c062fe6a4a7fd5d0855ee87dd72a3bfe098395
Matthew Mateo <mmatteo36@gmail.com>
2018-11-23 18:07:16 -0800

    modis

--- b_libft/srcs/ft_strncpy.c
+++ b_libft/srcs/ft_strncpy.c
@@ -6,27 +6,23 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 17:53:07 by mamateo           #+#    #+#             */
-/*   Updated: 2018/11/22 02:02:28 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/11/23 18:06:09 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t len)
+char		*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	size_t i;
+	char	*poop;
 
-	i = 0;
-	while (src[i] != '\0' && i < len)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	while (i < len)
-	{
-		dst[i] = '\0';
-		i++;
-	}
-	return (dst);
+	poop = dst;
+	while (len--)
+		if ((*dst++ = *src++) == '\0')
+		{
+			while (len--)
+				*dst++ = '\0';
+			return (poop);
+		}
+	return (poop);
 }
