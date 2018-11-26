e71c9c5cb6df6b0c387b20d2ad98460bf1d2e6d6
Matthew Mateo <mmatteo36@gmail.com>
2018-11-25 23:26:42 -0800

    modifications to strnstr

--- b_libft/srcs/ft_strnstr.c
+++ b_libft/srcs/ft_strnstr.c
@@ -6,26 +6,26 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 16:32:31 by mamateo           #+#    #+#             */
-/*   Updated: 2018/11/10 17:40:20 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/11/25 22:48:30 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *substr, size_t len)
+char		*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-	size_t	i;
+	char	*needle;
+	size_t	l;
 
-	i = 0;
-	len = substr[i] != '\0';
-	if (!substr)
-		return ((char *)str);
-	while (((i + len) <= len) && *str)
+	needle  = (char *)s2;
+	l = ft_strlen(needle);
+	if (*needle == '\0')
+		return ((char *)s1);
+	while (*s1 && len-- >= l)
 	{
-		if (ft_memcmp(str, substr, len) == 0)
-			return (char *)(str);
-		str++;
-		i++;
+		if ((*s1 == *needle) && (ft_strncmp(s1, needle, l) == 0))
+			return ((char *)s1);
+		s1++;
 	}
 	return (NULL);
 }
