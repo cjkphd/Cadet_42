7849f30d18151c8ed2e459437f97e9c22e50007d
Matthew Mateo <mmatteo36@gmail.com>
2018-11-26 00:06:05 -0800

    modi strnew

--- b_libft/srcs/ft_strnew.c
+++ b_libft/srcs/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 22:09:05 by mamateo           #+#    #+#             */
-/*   Updated: 2018/11/08 22:11:37 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/11/26 00:05:16 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 char		*ft_strnew(size_t size)
 {
-	char	*str;
-
-	str = (char*)malloc(size);
-	if (str == NULL)
-		return (NULL);
-	ft_memset(str, (int)'\0', size + 1);
-	return (str);
+	return ((char *)ft_memalloc(size + 1));
 }
+
+/*
+** ft_memalloc -- allocates and returns a “fresh” memory area.
+** The memory allocated is initialized to 0.
+** If the allocation fails, the function returns NULL.
+*/
